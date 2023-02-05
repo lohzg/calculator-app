d0b711206a409b4b00bdd25f96ccf9b1b984b982
Zen <zguang.loh@gmail.com>
2023-02-06 00:48:00 +0800

    second commit

--- script.js
+++ script.js
@@ -1,56 +1,22 @@
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("input[type='button']");
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
-  button.addEventListener("click", function() {
-    let value = this.value;
-    switch (value) {
-      case "C":
-        display.value = "";
-        break;
-      case "=":
-        try {
-          display.value = evalExpression(display.value);
-        } catch (error) {
-          display.value = "Error";
-        }
-        break;
-      default:
-        display.value += value;
-        break;
+  button.addEventListener('click', event => {
+    const value = event.target.textContent;
+
+    if (!isNaN(value) || value === '.') {
+      display.textContent += value;
+    } else if (value === 'C') {
+      display.textContent = '';
+    } else if (value === '=') {
+      try {
+        display.textContent = eval(display.textContent);
+      } catch (error) {
+        display.textContent = 'Error';
+      }
+    } else {
+      display.textContent += value;
     }
   });
 });
-
-function evalExpression(expression) {
-  expression = expression.replace(/\s/g, "");
-  const operators = ["+", "-", "*", "/"];
-
-  for (let i = 0; i < operators.length; i++) {
-    while (expression.includes(operators[i])) {
-      let split = expression.split(operators[i]);
-      let a = parseFloat(split[0]);
-      let b = parseFloat(split[1]);
-      if (isNaN(a) || isNaN(b)) {
-        throw new Error();
-      }
-      let result;
-      switch (operators[i]) {
-        case "+":
-          result = a + b;
-          break;
-        case "-":
-          result = a - b;
-          break;
-        case "*":
-          result = a * b;
-          break;
-        case "/":
-          result = a / b;
-          break;
-      }
-      expression = expression.replace(`${a}${operators[i]}${b}`, result);
-    }
-  }
-  return expression;
-}
